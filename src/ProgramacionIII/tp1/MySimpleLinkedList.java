9beb034723f7a31c20d288fc1bc4cf5ff2bdc25c
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-04-12 13:24:48 -0300

    implementacion metodos insertFront, extractFront, isEmpty, size, get, indexOf - MySimpleLinkedList class- dev branch

--- src/ProgramacionIII/tp1/MySimpleLinkedList.java
+++ src/ProgramacionIII/tp1/MySimpleLinkedList.java
@@ -3,35 +3,68 @@ package ProgramacionIII.tp1;
 public class MySimpleLinkedList {
 	
 	protected Node first;
+	private int size;
 	
 	public MySimpleLinkedList() {
 		this.first = null;
+		this.size = 0;
 	}
 	
 	public void insertFront(Object o) {
 		Node tmp = new Node(o,null);
 		tmp.setNext(this.first);
 		this.first = tmp;
+		this.size++;
 	}
 	
 	public Object extractFront() {
 		// TODO
-		return null;
+		Node tmp = this.first;
+		this.first = this.first.getNext();
+		this.size--;
+		return tmp.getInfo();
 	}
 
 	public boolean isEmpty() {
 		// TODO
-		return false;
+		return this.first == null;
 	}
 
 	public int size() {
 		// TODO
-		return 0;
+		return this.size;
 	}
 	
 	public Object get(int index) {
 		// TODO
-		return null;
+		int i = 0;
+		Node tmp = this.first;
+		
+		while(index < this.size && i < index) {
+			tmp = tmp.getNext();
+			i++;
+		}
+		
+		if(index < this.size) {
+			return tmp.getInfo();
+		} else {
+			return null;
+		}
 	}
 
+	public int indexOf(Object o) {
+		int index = 0;
+		Node tmp = this.first;
+		
+		while(tmp != null && !(tmp.getInfo().equals(o))) {
+			tmp = tmp.getNext();
+			index++;
+		}
+		
+		if(tmp != null) {
+			return index;
+		} else {
+			return -1;
+		}
+	}
 }
