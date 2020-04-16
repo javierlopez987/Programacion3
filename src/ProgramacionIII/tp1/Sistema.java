a8bea02ff4451163eb7f8ef9f2cf839fa8b512e0
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-04-16 11:07:57 -0300

    listar comunes desordenados, alternativas for e iterador - Sistema class - master branch

--- src/ProgramacionIII/tp1/Sistema.java
+++ src/ProgramacionIII/tp1/Sistema.java
@@ -15,10 +15,11 @@ public class Sistema {
 		MySimpleLinkedList lista1 = new MySimpleLinkedList();
 		MySimpleLinkedList lista2 = new MySimpleLinkedList();
 		MySimpleLinkedList lista3 = new MySimpleLinkedList();
+		MySimpleLinkedList lista4 = new MySimpleLinkedList();
 		
 		//reloj.start();
-		cargarOrdenado(lista1);
-		cargarOrdenado(lista2);
+		cargarAleatorio(lista1);
+		cargarAleatorio(lista2);
 		System.out.println("Lista 1");
 		imprimir(lista1);
 		System.out.println("Lista 2");
@@ -26,14 +27,19 @@ public class Sistema {
 		//System.out.println(reloj.stop());
 		//reloj.start();
 		System.out.println("Lista resultado");
-		lista3 = listarContienePrimeraNoSegunda(lista1, lista2);
+		//lista3 = listarContienePrimeraNoSegunda(lista1, lista2);
 		//System.out.println(reloj.stop());
 		//reloj.start();
-		lista3 = lista3.reverse();
+		//lista3 = lista3.reverse();
 		//System.out.println(reloj.stop());
-		//reloj.start();
+		reloj.start();
+		lista3 = listarComunesDesordenadosIt(lista1, lista2);
+		System.out.println(reloj.stop());
 		imprimir(lista3);
-		//System.out.println(reloj.stop());
+		reloj.start();
+		lista4 = listarComunesDesordenadosFor(lista1, lista2);
+		System.out.println(reloj.stop());
+		imprimir(lista4);
 		
 	}
 	
@@ -106,4 +112,37 @@ public class Sistema {
 		return result;
 	}
 	
+	public static MySimpleLinkedList listarComunesDesordenadosIt(MySimpleLinkedList lista1, MySimpleLinkedList lista2) {
+		MySimpleLinkedList result = new MySimpleLinkedList();
+		
+		Integer aux;
+		IteradorSimple it1 = lista1.iterator();
+		
+		while(it1.hasNext()) {
+			aux = it1.next();
+			IteradorSimple it2 = lista2.iterator();
+			
+			while(it2.hasNext()) {
+				if(aux.equals(it2.next())) {
+					result.insertFront(aux);
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public static MySimpleLinkedList listarComunesDesordenadosFor(MySimpleLinkedList lista1, MySimpleLinkedList lista2) {
+		MySimpleLinkedList result = new MySimpleLinkedList();
+		
+		for (Integer element1: lista1) {
+			for (Integer element2: lista2) {
+				if(element1 == element2) {
+					result.insertFront(element1);
+				}
+			}
+		}
+		
+		return result;
+	}
 }
