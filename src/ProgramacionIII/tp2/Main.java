48cea4469be94a754679c563466660eb0d5b3892
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-04-29 10:27:12 -0300

    implementacion test3 prueba delete - Main class - tp2Entregable branch

--- src/ProgramacionIII/tp2/Main.java
+++ src/ProgramacionIII/tp2/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Tree arbol = new Tree(50);
 		
-		procesarTest1(arbol);
+		//procesarTest1(arbol);
 		//procesarTest2(arbol);
+		procesarTest3(arbol);
 		
 	}
 	
@@ -44,14 +45,19 @@ public class Main {
 		//System.out.println(arbol.hasElem(99));
 		//System.out.println(arbol.hasElem(9));
 		
+		System.out.println("getHeight():");
 		System.out.println(arbol.getHeight());
 		
+		System.out.println("getFrontera():");
 		System.out.println(arbol.getFrontera());
 		
+		System.out.println("getLongestBranch():");
 		System.out.println(arbol.getLongestBranch());
 		
+		System.out.println("getElemAtLevel(4):");
 		System.out.println(arbol.getElemAtLevel(4));
 		
+		System.out.println("getElemAtLevel(8):");
 		System.out.println(arbol.getElemAtLevel(8));
 		
 		//arbol.delete(45);
@@ -137,6 +143,54 @@ public class Main {
 		
 		System.out.println(arbol.getElemAtLevel(4));
 		
+		System.out.println(arbol.getElemAtLevel(8));
+	}
+	
+	public static void procesarTest3(Tree arbol) {
+		arbol.add(80);
+		arbol.add(99);
+		arbol.add(60);
+		
+		System.out.println("printInOrder():");
+		arbol.printInOrder();
+		System.out.println();
+		
+		System.out.println("getHeight():");
+		System.out.println(arbol.getHeight());
+		
+		System.out.println("getFrontera():");
+		System.out.println(arbol.getFrontera());
+		
+		System.out.println("getLongestBranch():");
+		System.out.println(arbol.getLongestBranch());
+		
+		System.out.println("getElemAtLevel(4):");
+		System.out.println(arbol.getElemAtLevel(4));
+		
+		System.out.println("getElemAtLevel(8):");
+		System.out.println(arbol.getElemAtLevel(8));
+		
+		//arbol.delete(45);
+		arbol.delete(50);
+		
+		System.out.println("printInOrder():");
+		arbol.printInOrder();
+		System.out.println();
+		
+		
+		System.out.println("getHeight():");
+		System.out.println(arbol.getHeight());
+		
+		System.out.println("getFrontera():");
+		System.out.println(arbol.getFrontera());
+		
+		System.out.println("getLongestBranch():");
+		System.out.println(arbol.getLongestBranch());
+		
+		System.out.println("getElemAtLevel(4):");
+		System.out.println(arbol.getElemAtLevel(4));
+		
+		System.out.println("getElemAtLevel(8):");
 		System.out.println(arbol.getElemAtLevel(8));
 	}
 }
