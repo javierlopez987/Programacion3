17c2773346c9410ef06dc546ccf5e64706ebc690
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-05-08 18:13:03 -0300

    implementacion constructor con Map(vertices) y List(aristas) - GrafoDirigido class - tp3Entregable package

--- src/ProgramacionIII/tp3Entregable/GrafoDirigido.java
+++ src/ProgramacionIII/tp3Entregable/GrafoDirigido.java
@@ -1,9 +1,22 @@
 package ProgramacionIII.tp3Entregable;
 
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class GrafoDirigido<T> implements Grafo<T> {
-
+	private Map<Integer, List<Arco<T>>> vertices;
+	private List<Arco<T>> aristas;
+	
+	
+	public GrafoDirigido() {
+		// TODO Auto-generated constructor stub
+		this.vertices = new LinkedHashMap<Integer, List<Arco<T>>>();
+		this.aristas = new LinkedList<Arco<T>>();
+	}
+	
 	@Override
 	public void agregarVertice(int verticeId) {
 		// TODO Auto-generated method stub
