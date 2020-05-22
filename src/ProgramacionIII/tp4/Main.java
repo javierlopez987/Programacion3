94164a2f30a0ee34eb966c121bc2b86b78d0e0a2
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-05-22 13:33:17 -0300

    implementacion ej1 billetes - Main - tp4 package

--- src/ProgramacionIII/tp4/Main.java
+++ src/ProgramacionIII/tp4/Main.java
@@ -1,17 +1,18 @@
 package ProgramacionIII.tp4;
 
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 	private static Integer W;
-	private static Integer S;
+	private static Integer S; //S = variable que acumula y controla soluciones parciales
 	
 	public static void main(String[] args) {
 		W = 289;
-		List<Integer> resultado;
+		Map<Integer, Integer> resultado;
 		
 		List<Integer> monedas = new LinkedList<>();
 		monedas.add(100);
@@ -22,46 +23,51 @@ public class Main {
 
 		resultado = greedyMonedas(monedas);
 		System.out.println(resultado);
-		System.out.println(resultado.size());
 	}
 
-	public static List<Integer> greedyMonedas(List<Integer> C) {
-		Integer S;
-		Integer x;
+	public static Map<Integer, Integer> greedyMonedas(List<Integer> C) {
+		Map<Integer, Integer> solucion = new LinkedHashMap<>(); // Guarda soluciones
+		Integer x; //x = billete
 		
 		S = W; // Lo que la teoría conoce como "Solución inicial vacía"
 		
-		Collections.sort(C); //Ordenar lista para mejorar rendimiento
+		Collections.sort(C, Collections.reverseOrder()); //Ordenar lista para mejorar rendimiento
 		
 		while(!C.isEmpty() && !solucion(S)) {
 			x = seleccionar(C);
 			C.remove(x);
 			
 			if(factible(x, S)) // es factible explicito
-				agregar(x, S); //
-			else
-				S.add(0);
+				agregar(x, solucion); // agregar candidato seleccionado a solución parcial
 		}
 		
 		if(solucion(S)) {
-			return S;
+			return solucion;
 		} else {
 			return null;
 		}
 	}
 	
-	private static boolean factible(Integer x, Integer p2) {
+	private static boolean factible(Integer x, Integer S) {
 		
-		return x <= p2;
+		return x <= S;
 	}
 
-	private static void agregar(int x, List<Integer> S) {
+	private static void agregar(Integer x, Map<Integer, Integer> solucion) {
+
+		int aux = 0;
+		int veces = 1;
 		
-		int aux = S/x; //cantidad de veces que entra el billete
+		if(x != 0)
+			veces = S/x; //cantidad de veces que entra el billete
 		
-		S.add(aux);
+		//S.add(aux);
 		
-		S -= aux * x; 
+		aux = veces * x;
+		
+		S -= aux; //Se agrega
+		
+		solucion.put(x, veces);
 		
 		/*
 		while (x <= P) {
@@ -74,7 +80,9 @@ public class Main {
 
 	private static int seleccionar(List<Integer> candidatos) {
 		int max = 0; // max => billete de mayor denominacion
-		int aux = 0;
+		/*
+		 * int aux = 0;
+		 */
 		
 		
 		// No recorrer candidatos si los llevo ordenados
@@ -88,12 +96,15 @@ public class Main {
 			}
 		}
 		*/
+		if(!candidatos.isEmpty()) {
+			max = candidatos.get(0);
+		}
 		
 		return max;
 	}
 
-	private static boolean solucion(List<Integer> S) {
-		return S.get(0) == 0;
+	private static boolean solucion(Integer S) {
+		return S == 0;
 		/*
 		int suma = 0;
 		Iterator<Integer> it = S.iterator();
